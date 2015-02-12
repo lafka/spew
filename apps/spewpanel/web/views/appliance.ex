2f7aff532bd89d45ae2e8fd5b056d6e24084df09
Olav Frengstad <olav@fwt.no>
2015-02-12 18:33:44 +0100

    Add appliance information and linking to builds

--- apps/spewpanel/web/views/appliance.ex
+++ apps/spewpanel/web/views/appliance.ex
@@ -1,3 +1,9 @@
 defmodule Spewpanel.ApplianceView do
   use Spewpanel.Web, :view
+
+  def buildinfo({nil, nil}), do: "No builds"
+  def buildinfo({type, nil}), do: "#{type}, no spec"
+  def buildinfo({"spew-archive-1.0", spec}), do:
+    link("#{spec["TARGET"]}/#{spec["VSN"]}", to: "/build/#{spec["CHECKSUM"]}")
+  def buildinfo({type, _spec}), do: "#{type}, unknown provider"
 end
