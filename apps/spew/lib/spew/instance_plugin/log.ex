e9fdbd293e9ccb710f7fe3bce829f0232fc7bf32
Olav Frengstad <olav@fwt.no>
2015-06-30 14:50:35 +0200

    Make InstancePlugin.Log not run

--- apps/spew/lib/spew/instance_plugin/log.ex
+++ apps/spew/lib/spew/instance_plugin/log.ex
@@ -1,3 +1,24 @@
 defmodule Spew.InstancePlugin.Log do
+  @moduledoc """
+  Add support for logging io of
+  """
+
+  require Logger
+
+  alias Spew.Instance.Item
+
+  def setup(_instance, _) do
+    raise Exception, message: "unsupported plugin"
+  end
+
+  @doc """
+  Start the log plugin, currently unsupported
+  """
+  def start(%Item{ref: ref,
+                  plugin: %{__MODULE__ => state}} = instance, opts) do
+    raise Exception, message: "unsupported plugin"
+  end
+
+  def event(%Item{plugin: %{ __MODULE__ => _}}, state, _ev), do: state
 end
 
